fb2b69103541b2259ed97d6343f0c35d36e74e81
Charles Lindsay <chaz@chazomatic.us>
2014-02-03 09:39:17 -0800

    Run shutdown script, wait for it

--- init.c
+++ init.c
@@ -5,19 +5,26 @@
 #include <signal.h>
 #include <sys/wait.h>
 
-#ifndef DEFAULT_RC
-    #define DEFAULT_RC "/etc/rc.init"
+#ifndef DEFAULT_STARTUP
+    #define DEFAULT_STARTUP "/etc/rc.startup"
+#endif
+#ifndef DEFAULT_SHUTDOWN
+    #define DEFAULT_SHUTDOWN "/etc/rc.shutdown"
 #endif
 
 
+static volatile pid_t shutdown_pid = 0;
 static volatile int terminate = 0;
 
 
 static void handle_child(int sig) {
     int saved_errno = errno;
 
-    while(waitpid(-1, NULL, WNOHANG) > 0)
-        continue;
+    pid_t pid;
+    while((pid = waitpid(-1, NULL, WNOHANG)) > 0) {
+        if(pid == shutdown_pid)
+            shutdown_pid = 0;
+    }
 
     errno = saved_errno;
 }
@@ -43,7 +50,7 @@ static sigset_t setup_signals(sigset_t *out_default_mask) {
     sigdelset(&wait_mask, SIGTERM);
     sigdelset(&wait_mask, SIGINT);
 
-    // TODO: also handle SIGUSR1 or such and send it to the rc process?
+    // TODO: also handle SIGUSR1, maybe run another script?
 
     sigset_t all_mask;
     sigfillset(&all_mask);
@@ -69,10 +76,16 @@ int main(int argc, char *argv[]) {
     sigset_t default_mask;
     sigset_t wait_mask = setup_signals(&default_mask);
 
-    const char *rc = (argc > 1 ? argv[1] : NULL);
-    run((rc && rc[0] ? rc : DEFAULT_RC), default_mask);
+    const char *startup = (argc > 1 && *argv[1] ? argv[1] : DEFAULT_STARTUP);
+    const char *shutdown = (argc > 2 && *argv[2] ? argv[2] : DEFAULT_SHUTDOWN);
+
+    run(startup, default_mask);
 
     while(!terminate)
+        sigsuspend(&wait_mask);
+
+    shutdown_pid = run(shutdown, default_mask);
+    while(shutdown_pid > 0)
         sigsuspend(&wait_mask);
 
     // Only bring everything else down when we're actually init.
