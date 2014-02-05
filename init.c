d91eb5be19f66be1af400369d7604ba1060c2032
Charles Lindsay <chaz@chazomatic.us>
2014-02-04 21:43:49 -0800

    Rename for clarity

--- init.c
+++ init.c
@@ -37,25 +37,25 @@ static sigset_t setup_signals(sigset_t *out_default_mask) {
     sigfillset(&all_mask);
     sigprocmask(SIG_SETMASK, &all_mask, out_default_mask);
 
-    sigset_t wait_mask;
-    sigfillset(&wait_mask);
+    sigset_t suspend_mask;
+    sigfillset(&suspend_mask);
 
     struct sigaction action = { .sa_flags = SA_NOCLDSTOP | SA_RESTART };
     sigfillset(&action.sa_mask);
 
     action.sa_handler = handle_child;
     sigaction(SIGCHLD, &action, NULL);
-    sigdelset(&wait_mask, SIGCHLD);
+    sigdelset(&suspend_mask, SIGCHLD);
 
     action.sa_handler = handle_termination;
     sigaction(SIGTERM, &action, NULL);
     sigaction(SIGINT, &action, NULL);
-    sigdelset(&wait_mask, SIGTERM);
-    sigdelset(&wait_mask, SIGINT);
+    sigdelset(&suspend_mask, SIGTERM);
+    sigdelset(&suspend_mask, SIGINT);
 
     // TODO: also handle SIGUSR1, maybe run another script?
 
-    return wait_mask;
+    return suspend_mask;
 }
 
 static pid_t run(const char *filename, sigset_t child_mask) {
@@ -73,7 +73,7 @@ static pid_t run(const char *filename, sigset_t child_mask) {
 
 int main(int argc, char *argv[]) {
     sigset_t default_mask;
-    sigset_t wait_mask = setup_signals(&default_mask);
+    sigset_t suspend_mask = setup_signals(&default_mask);
 
     const char *startup = (argc > 1 && *argv[1] ? argv[1] : DEFAULT_STARTUP);
     const char *shutdown = (argc > 2 && *argv[2] ? argv[2] : DEFAULT_SHUTDOWN);
@@ -81,11 +81,11 @@ int main(int argc, char *argv[]) {
     run(startup, default_mask);
 
     while(!terminate)
-        sigsuspend(&wait_mask);
+        sigsuspend(&suspend_mask);
 
     shutdown_pid = run(shutdown, default_mask);
     while(shutdown_pid > 0)
-        sigsuspend(&wait_mask);
+        sigsuspend(&suspend_mask);
 
     // Only bring everything else down when we're actually init.
     if(getpid() == 1) {
