60f704b23f5fd5227a8fe7974698014d752d2415
Charles Lindsay <chaz@chazomatic.us>
2015-03-21 13:11:49 -0700

    Simplify implementation
    Don't need to juggle signals at all, just rely on wait's normal
    behavior.

--- ex-it.c
+++ ex-it.c
@@ -20,17 +20,12 @@
 */
 
 #include <errno.h>
-#include <signal.h>
-#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
 #include <unistd.h>
 
-#if _POSIX_C_SOURCE < 199309L
-#error ex-it requires _POSIX_C_SOURCE >= 199309
-#endif
 
 enum {
     E_ARGS = 0x101, // > 0xff (max exit code from child), & 0xff != 0.
@@ -41,36 +36,20 @@ enum {
 };
 
 
-static int handle_child(pid_t search_pid) {
-    int search_status = -1;
-
-    int status;
-    for(pid_t pid; (pid = waitpid(-1, &status, WNOHANG)) > 0; ) {
-        if(pid == search_pid) {
+static int wait_for_child(pid_t child_pid) {
+    while(1) {
+        int status;
+        if(wait(&status) == child_pid) {
             if(WIFEXITED(status))
-                search_status = WEXITSTATUS(status);
-            else if(WIFSIGNALED(status))
-                search_status = E_SIGNALED_START + WTERMSIG(status);
-            else
-                search_status = E_UNKNOWN;
+                return WEXITSTATUS(status);
+            if(WIFSIGNALED(status))
+                return E_SIGNALED_START + WTERMSIG(status);
+            return E_UNKNOWN;
         }
     }
-
-    return search_status;
 }
 
-static int wait_for_child(pid_t child_pid) {
-    sigset_t receive_set;
-    sigemptyset(&receive_set);
-    sigaddset(&receive_set, SIGCHLD);
-
-    int status;
-    for(status = -1; status < 0; status = handle_child(child_pid))
-        sigwaitinfo(&receive_set, NULL);
-    return status;
-}
-
-static pid_t run_args(char *argv[], sigset_t child_mask) {
+static pid_t run_args(char *argv[]) {
     pid_t pid = fork();
     if(pid == -1) {
         fprintf(stderr, "%s: fork: %s\n", argv[0], strerror(errno));
@@ -78,7 +57,6 @@ static pid_t run_args(char *argv[], sigset_t child_mask) {
     }
 
     if(pid == 0) {
-        sigprocmask(SIG_SETMASK, &child_mask, NULL);
         execvp(argv[1], &argv[1]);
 
         fprintf(stderr, "%s: exec %s: %s\n", argv[0], argv[1],
@@ -89,21 +67,11 @@ static pid_t run_args(char *argv[], sigset_t child_mask) {
     return pid;
 }
 
-static sigset_t block_signals(void) {
-    sigset_t block_set;
-    sigfillset(&block_set);
-
-    sigset_t old_mask;
-    sigprocmask(SIG_SETMASK, &block_set, &old_mask);
-
-    return old_mask;
-}
-
 int main(int argc, char *argv[]) {
     if(argc < 2) {
         fprintf(stderr, "%s: too few arguments\n", argv[0]);
         return E_ARGS;
     }
 
-    return wait_for_child(run_args(argv, block_signals()));
+    return wait_for_child(run_args(argv));
 }
